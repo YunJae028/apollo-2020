b651547102814f207ff6651b1dbe5c2714a44a11
yunaje028 <tjdbswo7@gmail.com>
2020-10-24 12:42:05 +0900

    apollo - 2020

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import client from "./apollo";
+import { ApolloProvider } from "@apollo/react-hooks";
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+<ApolloProvider client = {client}>
+<App />
+</ApolloProvider>
+,document.getElementById('root'));
